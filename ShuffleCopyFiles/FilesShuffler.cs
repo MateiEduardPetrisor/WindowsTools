3e7b7285b6b3c2b571dcb1eeec5a64822f8f7bfe
Matei Eduard-Petrisor <matei.eduard.petrisor@gmail.com>
2023-03-09 22:30:17 +0200

    Update FilesShuffler.cs
    Refactoring to split the files in multiple folders using a filesperfolder. Some car stereo will not read more than 256 files in a single folder and in this case you have to split the music collection on multiple folders.

--- ShuffleCopyFiles/FilesShuffler.cs
+++ ShuffleCopyFiles/FilesShuffler.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.IO;
 using System.Security.Cryptography;
+using System.Collections.Generic;
 
 namespace ShuffleCopyFiles
 {
@@ -9,81 +10,145 @@ namespace ShuffleCopyFiles
     {
         private readonly String SourceFullPath;
         private readonly String DestinationFullPath;
+        private readonly Int32 FilesPerFolder;
 
-        public FilesShuffler(String SourceFullPth, String DestinationFullPth)
+        public FilesShuffler(String SourceFullPth, String DestinationFullPth, Int32 FilesPerFolder)
         {
             this.SourceFullPath = SourceFullPth;
             this.DestinationFullPath = DestinationFullPth;
+            this.FilesPerFolder = FilesPerFolder;
+
             Console.WriteLine("Soruce Folder Is {0}", this.SourceFullPath);
             Console.WriteLine("Destination Folder Is {0}", this.DestinationFullPath);
-            DirectoryInfo SourceDirInfo = new DirectoryInfo(this.SourceFullPath);
-            DirectoryInfo DestinationDirInfo = new DirectoryInfo(this.DestinationFullPath);
-            if (SourceDirInfo.Exists)
+            Console.WriteLine("FilesPerFolder Is {0}", this.FilesPerFolder);
+        }
+
+        private bool CheckIfFolderExist(String FullPath)
+        {
+            DirectoryInfo FolderToCheck = new DirectoryInfo(FullPath);
+            if (FolderToCheck.Exists)
             {
-                if (!DestinationDirInfo.Exists)
-                {
-                    Console.WriteLine("Create Directory {0}", this.DestinationFullPath);
-                    Directory.CreateDirectory(this.DestinationFullPath);
-                }
+                return true;
             }
             else
             {
-                Console.WriteLine("Source Directory {0} Doesn't Exist!", this.SourceFullPath);
+                Console.WriteLine("Direcory {0} does not exist", FolderToCheck.FullName);
+                return false;
             }
+        }
+
+        private Int32 GetNumberOfFilesInSourceFolder()
+        {
+            Int32 NumberOfFiles = 0;
+            if (this.CheckIfFolderExist(this.SourceFullPath))
+            {
+                NumberOfFiles = Directory.GetFiles(SourceFullPath, "*.mp3", SearchOption.AllDirectories).Count();
+            }
+            return NumberOfFiles;
+        }
+
+        private String[] GetFileArray()
+        {
+            String[] FilesArrayObj = null;
+            if (this.CheckIfFolderExist(this.SourceFullPath))
+            {
+                FilesArrayObj = Directory.GetFiles(SourceFullPath, "*.mp3", SearchOption.AllDirectories);
+            }
+            return FilesArrayObj;
         }
 
         private String[] ShuffleFileList()
         {
-            String[] FilesArrayObj = Directory.GetFiles(SourceFullPath, "*.mp3", SearchOption.AllDirectories);
+            Int32 NumberOfFiles = this.GetNumberOfFilesInSourceFolder();
+            String[] FilesArrayObj = this.GetFileArray();
             RNGCryptoServiceProvider RngProvider = new RNGCryptoServiceProvider();
-            Int32 ArraySize = FilesArrayObj.Count();
-            Console.WriteLine("Found {0} .mp3 Files", ArraySize);
-            while (ArraySize > 1)
+            Console.WriteLine("Found {0} .mp3 Files", NumberOfFiles);
+            while (NumberOfFiles > 1)
             {
                 Byte[] ByteArrayObj = new Byte[4];
                 do
                 {
                     RngProvider.GetBytes(ByteArrayObj);
                 }
-                while (!(BitConverter.ToInt32(ByteArrayObj, 0) < ArraySize * (Int32.MaxValue / ArraySize)));
-                Int32 NewIndex = BitConverter.ToInt32(ByteArrayObj, 0) % ArraySize;
+                while (!(BitConverter.ToInt32(ByteArrayObj, 0) < NumberOfFiles * (Int32.MaxValue / NumberOfFiles)));
+                Int32 NewIndex = BitConverter.ToInt32(ByteArrayObj, 0) % NumberOfFiles;
                 if (NewIndex <= 0)
                 {
                     NewIndex *= (-1);
                 }
-                --ArraySize;
+                --NumberOfFiles;
                 String TempFileName = FilesArrayObj[NewIndex];
-                FilesArrayObj[NewIndex] = FilesArrayObj[ArraySize];
-                FilesArrayObj[ArraySize] = TempFileName;
+                FilesArrayObj[NewIndex] = FilesArrayObj[NumberOfFiles];
+                FilesArrayObj[NumberOfFiles] = TempFileName;
             }
             RngProvider.Dispose();
             Console.WriteLine("FilesArray Shuffled!");
             return FilesArrayObj;
         }
 
-        public void CopyFiles(Boolean Overwrite)
+        private List<String> CreateFolders()
         {
-            String[] FileArrayObj = this.ShuffleFileList();
-            if (FileArrayObj != null)
+            List<String> Folders = new List<String>();
+            Int32 NumberOfFoldersToCreate = 0;
+            if (this.GetNumberOfFilesInSourceFolder() % this.FilesPerFolder != 0)
             {
-                for (Int32 i = 0; i < FileArrayObj.Count(); i++)
-                {
-                    Console.WriteLine("Copy {0} File {1} To {2}", i + 1, FileArrayObj.ElementAt(i), this.DestinationFullPath);
-                    FileInfo FileInfoObj = new FileInfo(FileArrayObj.ElementAt(i));
-                    if (FileInfoObj.Exists)
-                    {
-                        File.Copy(FileArrayObj.ElementAt(i), Path.Combine(this.DestinationFullPath, FileInfoObj.Name), Overwrite);
-                    }
-                    else
-                    {
-                        Console.WriteLine("File {0} Doesn't Exist!", FileArrayObj.ElementAt(i));
-                    }
-                }
-                Console.WriteLine("Copy Of Files Completed!");
+                NumberOfFoldersToCreate++;
             }
-            else
+            NumberOfFoldersToCreate += this.GetNumberOfFilesInSourceFolder() / this.FilesPerFolder;
+            if (!this.CheckIfFolderExist(this.DestinationFullPath))
             {
-                Console.WriteLine("FilesArray Is Null Nothing To Copy!");
+                for (Int32 i = 1; i <= NumberOfFoldersToCreate; i++)
+                {
+                    String NewDirectoryFullPath = this.DestinationFullPath + "_" + i;
+                    Directory.CreateDirectory(NewDirectoryFullPath);
+                    Folders.Add(NewDirectoryFullPath);
+                }
+            }
+            return Folders;
+        }
+
+        public void CopyFiles()
+        {
+            List<String> Folders = CreateFolders();
+            String[] FileArrayObj = this.ShuffleFileList();
+            Int32 IndexCurrentFolder = 0;
+            Int32 NumberOfCopiedFiles = 0;
+            while (IndexCurrentFolder < Folders.Count)
+            {
+                if (FileArrayObj != null)
+                {
+                    for (Int32 IndexCurrentFile = 0; IndexCurrentFile < FileArrayObj.Count(); IndexCurrentFile++)
+                    {
+                        Console.WriteLine("Copy {0} File {1} To {2}", IndexCurrentFile + 1, FileArrayObj.ElementAt(IndexCurrentFile), Folders.ElementAt(IndexCurrentFolder));
+                        FileInfo FileInfoObj = new FileInfo(FileArrayObj.ElementAt(IndexCurrentFile));
+                        if (FileInfoObj.Exists)
+                        {
+                            FileInfo FileInfoDest = new FileInfo(Path.Combine(Folders.ElementAt(IndexCurrentFolder), FileInfoObj.Name));
+                            if (FileInfoDest.Exists)
+                            {
+                                FileInfoDest.Attributes = FileAttributes.Normal;
+                            }
+                            File.Copy(FileArrayObj.ElementAt(IndexCurrentFile), Path.Combine(Folders.ElementAt(IndexCurrentFolder), FileInfoObj.Name), true);
+                            NumberOfCopiedFiles++;
+
+                            if (NumberOfCopiedFiles == this.FilesPerFolder)
+                            {
+                                IndexCurrentFolder += 1;
+                                NumberOfCopiedFiles = 0;
+                            }
+                            else if (IndexCurrentFile == FileArrayObj.Count() - 1)
+                            {
+                                NumberOfCopiedFiles = 0;
+                                IndexCurrentFolder += 1;
+                            }
+                        }
+                    }
+                    Console.WriteLine("Copy Of Files Completed!");
+                }
+                else
+                {
+                    Console.WriteLine("FilesArray Is Null Nothing To Copy!");
+                }
             }
         }
     }
